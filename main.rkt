a867307ebf3dce19255abeabba50e322251a0910
Alex.McGuire <alex.mcguire@trafigura.com>
2013-04-14 05:17:05 +0100

    Shuffle decks

--- main.rkt
+++ main.rkt
@@ -1,13 +1,37 @@
-#lang racket
+(module cards racket 
+  (require math)
+  (require racket/set)
+  (provide shuffle)
 
-(define cards 
-  (for*/list ([suit '(C D H S)]
-              [rank '(A 2 3 4 5 6 7 8 9 10 J Q K)])
-    (list rank suit)))
+  (random-seed 1234)
 
-(module+ test
-  (require rackunit)
-  (check-eq? 52 (length cards)))
+  (define (new-deck)
+    (for*/list ([suit '(C D H S)]
+                [rank '(A 2 3 4 5 6 7 8 9 10 J Q K)])
+      (list rank suit)))
 
+  (define (shuffle xs)
+    (let ([vec (list->vector xs)])
+      (for ([i (- (length xs) 1)]) 
+        (let ([j (random-integer i (- (length xs) 1))])
+;          (display (list i j))
+;          (display "\n")
+;          (display vec)
+;          (display "\n")
+          (let ([tmp (vector-ref vec i)])
+            (vector-set! vec i (vector-ref vec j))
+            (vector-set! vec j tmp))))
+      (vector->list vec)))
+
+  (define (shuffled-deck)
+    (shuffle (new-deck)))
+
+  (module+ test
+    (require rackunit)
+    (check-eq? 52 (length (new-deck)))
+    (check-eq? 52 (length (shuffled-deck)))
+    (check-eq? 52 (set-count (list->set (shuffled-deck))))
+    )
+)
              
 
