188557d810bb22ab8a20ec80e1cf2cf6706a499a
Alex.McGuire <alex.mcguire@trafigura.com>
2014-03-29 10:54:25 +0000

    More better CI

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 while [ true ]; do
   inotifywait -q -e modify *.lisp
   tmux set -q -g status-bg black
-  rlwrap sbcl --load play --eval '(in-package :cage433-cribbage)' --eval '(run-tests)'
+  rlwrap sbcl --noinform --load load --eval '(in-package :cage433-cribbage)' --eval '(compile-and-run-tests)'
   if [[ $? == 0 ]]; then
     tmux set -q -g status-bg blue
   else
--- load.lisp
+++ load.lisp
@@ -10,17 +10,27 @@
 
 (in-package :cage433-cribbage)
 
-(load-and-compile-if-necessary "utils")
-(load-and-compile-if-necessary "cards")
-(load-and-compile-if-necessary "game")
+(defun load-and-compile-source()
+  (format t "Loading source ~%")
+  (load-and-compile-if-necessary "utils")
+  (load-and-compile-if-necessary "cards")
+  ;(load-and-compile-if-necessary "game")
+  (load-and-compile-if-necessary "game2"))
 
-(defun run-tests(&key (exit-on-termination t))
-  (let ((result 
-          (combine-results (test-cards) 
-                           (test-game)
-                           ;(test-game2)
-                           (cage433-lisp-utils::run-tests :exit-on-termination nil)
-                           )))
-    (if exit-on-termination
-      (sb-ext:exit :code (if result 0 1))
-      result)))
+(defun compile-and-run-tests()
+  (declare #+sbcl(sb-ext:muffle-conditions style-warning))
+  (ignore-errors
+  (progn
+      (load-and-compile-source)
+      (let ((result 
+              (combine-results 
+                (test-cards) 
+                (test-game)
+                ;(test-game2)
+                (cage433-lisp-utils::run-tests :exit-on-termination nil)
+                )))
+        (sb-ext:exit :code (if result 0 1)))))
+  (format t "Badness happened~%~%")
+  (sb-ext:exit :code 1))
+
+    
