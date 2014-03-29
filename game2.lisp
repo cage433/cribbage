d46f6865cff4be12fe0a67a5a445d83074ee32e6
Alex.McGuire <alex.mcguire@trafigura.com>
2014-03-29 09:48:00 +0000

    Better CI

---
+++ game2.lisp
@@ -0,0 +1,65 @@
+(in-package :cage433-cribbage)
+
+(defstruct player
+  name
+  discard
+  choose-play-card
+  )
+
+(defstruct player-cards
+  deal
+  play-cards
+  crib-cards)
+
+(defmacro with-player-cards (player-cards &body body)
+  `(with-slots (deal play-cards crib-cards) ,player-cards
+    ,@body))
+
+(defstruct game-state
+  dealer
+  pone
+  dealer-cards
+  pone-cards
+  starter-card
+  score
+  play-cards
+  )
+
+(defmacro with-game-state (game-state &body body)
+  `(with-slots (dealer pone dealer-cards pone-cards starter-card score play-cards) ,game-state
+    ,@body))
+
+(defun initialise-game (dealer pone)
+  (let ((deck (shuffled-deck (make-random-state t)))
+        (game-state (make-game-state :dealer dealer :pone pone :dealer-cards (make-player-cards) :pone-cards (make-player-cards))))
+    (with-game-state game-state
+      (with-player-cards dealer-cards
+        (setf deal (subseq deck 0 6)))
+      (with-player-cards pone-cards
+        (setf deal (subseq deck 6 12)))
+      game-state)))
+
+(deftest test-game()
+  (let ((game-state (initialise-game nil nil)))
+    (with-game-state game-state
+      (with-player-cards dealer-cards
+        (check (= 6 (length deal))))
+      (with-player-cards pone-cards
+        (check (= 6 (length deal)))))))
+
+(defun deal (game-state) 
+  (declare (ignorable game-state))
+  'undefined)
+
+(defun play () 'undefined)
+(defun show () 'undefined)
+(defun discards () 'undefined)
+
+(defun play-a-game (game-state)
+  (deal game-state)
+  (discards)
+  (play)
+  (show))
+
+
+
