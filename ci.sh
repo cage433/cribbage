643878115147142bb5cb71e1442f6b420665db00
Alex McGuire <cage433@gmail.com>
2020-09-06 09:52:36 +0100

    Can ctrl-c ci-sh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 while [ true ]; do
-  fswatch -1 *.lisp play/*.lisp lisp-utils/*.lisp > /dev/null 2>&1
-  #inotifywait -q -e modify *.lisp play/*.lisp lisp-utils/*.lisp
-  echo "here"
+  # Weirdly, unless the command is wrapped in a $() we can't Ctrl-C the script
+  $(fswatch -1 *.lisp play/*.lisp lisp-utils/*.lisp > /dev/null 2>&1)
   tmux set -q -g status-bg black
   rlwrap sbcl --noinform --load load --eval '(in-package :cage433-cribbage)' --eval '(ci)'
   if [[ $? == 0 ]]; then
